eab6a2f7fe926f200eacc434ea5c3e90ed75bc51
sbwalker <shaun.walker@siliqon.com>
2025-09-05 13:45:13 -0400

    adjust logo height/width behavior

--- Client/Zephyr/ThemeInfo.cs
+++ Client/Zephyr/ThemeInfo.cs
@@ -9,7 +9,7 @@ namespace Oqtane.Theme.Bootswatch.Zephyr
         public Models.Theme Theme => new Models.Theme
         {
             Name = "Bootswatch Zephyr",
-            Version = "6.1.4",
+            Version = "6.1.5",
             ThemeSettingsType = "Oqtane.Theme.Bootswatch.ThemeSettings, Oqtane.Theme.Bootswatch.Oqtane",
             ContainerSettingsType = "Oqtane.Theme.Bootswatch.ContainerSettings, Oqtane.Theme.Bootswatch.Oqtane",
             PackageName = "Oqtane.Theme.Bootswatch",
